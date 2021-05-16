2f9f5c5330b15b6674c18f7a3772ad9c7859fbb7
Thiago Lopes <thiagoolopezz@gmail.com>
2021-05-16 15:15:25 -0300

    Update Service Tasks

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -4,11 +4,11 @@ import { Task } from './task';
 @Injectable()
 export class TaskService {
     tasks: Task[] = [
-        {id: 1, description: 'Tarefa 1', completed: false},
-        {id: 2, description: 'Tarefa 2', completed: false},
-        {id: 3, description: 'Tarefa 3', completed: false},
-        {id: 4, description: 'Tarefa 4', completed: false},
-        {id: 5, description: 'Tarefa 5', completed: false},
+        { id: 1, description: 'Tarefa 1', completed: false },
+        { id: 2, description: 'Tarefa 2', completed: false },
+        { id: 3, description: 'Tarefa 3', completed: true },
+        { id: 4, description: 'Tarefa 4', completed: false },
+        { id: 5, description: 'Tarefa 5', completed: false }
     ];
 
     getAll() {
@@ -17,11 +17,10 @@ export class TaskService {
     getById(id: number){
         const task = this.tasks.find((value) => value.id == id);
         return task;
-
     }
     create(task: Task){
         let lastId = 0;
-        if(this.tasks.length> 0) {
+        if (this.tasks.length > 0) {
             lastId = this.tasks[this.tasks.length - 1].id;
         }
         task.id = lastId + 1;
@@ -30,12 +29,11 @@ export class TaskService {
     }
     update(task: Task){
         const taskArray = this.getById(task.id);
-        if(taskArray) {
-            taskArray.description = taskArray.description;
+        if (taskArray) {
+            taskArray.description = task.description;
             taskArray.completed = task.completed;
         }
         return taskArray;
-
     }
     delete(id: number){
         const index = this.tasks.findIndex((value) => value.id == id);
